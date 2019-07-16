8fc2041389916c0399fb1ce7b733262a8fbcdc69
WJJWYF <45487976+WJJWYF@users.noreply.github.com>
2019-07-16 23:05:58 +0800

    通知的基本用法

---
+++ NotificationTest/app/src/main/java/com/example/notificationtest/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.notificationtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
+
+    String id="channel_1";
+    int importance=NotificationManager.IMPORTANCE_HIGH;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button sendNotice = (Button) findViewById(R.id.send_notice);
+        sendNotice.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.send_notice:
+                Intent intent = new Intent(this, NotificationActivity.class);
+                PendingIntent pi = PendingIntent.getActivity(this, 0, intent, 0);
+                NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                NotificationChannel mChannel=new NotificationChannel(id,"test",importance);
+                manager.createNotificationChannel(mChannel);
+                Notification notification = new NotificationCompat.Builder(this)
+                        .setContentTitle("This is content title")
+                        .setContentText("This is content text")
+                        .setWhen(System.currentTimeMillis())
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                        .setContentIntent(pi)
+                        .build();
+                manager.notify(1, notification);
+                break;
+            default:
+                break;
+        }
+    }
+
+}
