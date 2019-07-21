3380b445cd974ffc355fb42bd979ba8a2a0ff2d0
WJJWYF <45487976+WJJWYF@users.noreply.github.com>
2019-07-21 22:12:23 +0800

    通知(right)

--- NotificationTest/app/src/main/java/com/example/notificationtest/MainActivity.java
+++ NotificationTest/app/src/main/java/com/example/notificationtest/MainActivity.java
@@ -9,9 +9,13 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
 
@@ -29,18 +33,21 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.send_notice:
-                Intent intent = new Intent(this, NotificationActivity.class);
-                PendingIntent pi = PendingIntent.getActivity(this, 0, intent, 0);
+                Intent intent=new Intent(this,NotificationActivity.class);
+                PendingIntent pi=PendingIntent.getActivity(this,0,intent,0);
                 NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
                 NotificationChannel mChannel=new NotificationChannel(id,"test",importance);
                 manager.createNotificationChannel(mChannel);
-                Notification notification = new NotificationCompat.Builder(this)
+                Notification notification = new NotificationCompat.Builder(this,id)
                         .setContentTitle("This is content title")
                         .setContentText("This is content text")
                         .setWhen(System.currentTimeMillis())
                         .setSmallIcon(R.mipmap.ic_launcher)
                         .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
                         .setContentIntent(pi)
+                        .setVibrate(new long[]{0,1000,1000,1000})
+                        .setLights(Color.GREEN,1000,1000)
+                        .setStyle(new NotificationCompat.BigTextStyle().bigText("这是一段长文字ljdl ednw fenoeiw fewijfe nfewoi fewiowe oiwenoie owjfowie noifwen nwonoiw "))
                         .build();
                 manager.notify(1, notification);
                 break;
